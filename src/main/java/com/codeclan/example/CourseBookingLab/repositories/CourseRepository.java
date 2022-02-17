12b8d61fd379dbc4ba35b53db37e33fe9059c697
Adam Kidd <adamkidd3@gmail.com>
2022-02-17 12:18:09 +0000

    mvp finished

--- src/main/java/com/codeclan/example/CourseBookingLab/repositories/CourseRepository.java
+++ src/main/java/com/codeclan/example/CourseBookingLab/repositories/CourseRepository.java
@@ -4,6 +4,12 @@ import com.codeclan.example.CourseBookingLab.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findByStarRating(int starRating);
+
+    List<Course> findByBookingsCustomerName(String name);
 }
